4300ab4430ed87511a7c0b7d9bf675092957a6cf
Yusuf Mayet <jojo786@gmail.com>
2023-08-16 14:57:55 +0200

    Update multipart-file-upload-s3.sh

--- multipart-file-upload-s3.sh
+++ multipart-file-upload-s3.sh
@@ -2,17 +2,16 @@
 #
 #
 
-N=10
+#these values are passed in as command line arguments
+key=$1 #the original large file, e.g. 100GFile
+bucket=$2 #the S3 bucket to upload to
+N=$3 #number of parts to upload in parellel
 
-key=$1
-bucket=adaptittest1
-echo "Creating multipart upload_id: "
-
+echo "Step 1: Creating multipart upload_id"
 upload_id=`aws s3api create-multipart-upload --bucket ${bucket} --key $key | jq -r '.UploadId'`
 echo $upload_id
 
-echo "uploading parts concurrently in batches of N=" $10
-
+echo "Step 2: uploading parts concurrently in batches of N=" $N
 part_number=0
 
 #list files in the current directory that begin with x
@@ -25,8 +24,7 @@ do
   if [[ $(jobs -r -p | wc -l) -ge $N ]]; then wait -n; fi
 done
 
-
-echo "completing multipart upload"
+echo "Step 3: completing multipart upload"
 aws s3api complete-multipart-upload --multipart-upload file://upload-part-all.json --bucket ${bucket} --key ${key} --upload-id ${upload_id}
 
 echo "DONE"
